4f527d75ff36c12619026772aabee31ff049dd2d
MuriloMoraes <57009618+MuriloMoraes@users.noreply.github.com>
2020-03-11 19:11:14 -0300

    Create tabuada.java

---
+++ tabuada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class exercicio53
+{
+   public static void main(String[] args){
+       int n;
+       System.out.println(" Qual o seu numero: ");
+       n = new Scanner(System.in).nextInt();
+        for (int i = 1; i<11; i++)
+            System.out.println (n + " x " + i + " = " + n*i);
+       
+      
+    
+    }
+}
